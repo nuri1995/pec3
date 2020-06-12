c0890601c10d23c83f30ca53777fdebd7d46c6d2
nuri1995 <nuri.cruz.sanchez@gmail.com>
2020-06-13 01:40:30 +0200

    optimizacion imágenes

--- dist/pec3.e31bb0bc.js
+++ dist/pec3.e31bb0bc.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54180" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54317" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
